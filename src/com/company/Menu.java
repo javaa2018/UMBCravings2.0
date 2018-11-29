33b0d20f88df2d69a07da3250bf61f70e9613624
teamET147 <43768088+teamET147@users.noreply.github.com>
2018-11-29 17:48:36 -0500

    menu switch statement

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -14,7 +14,38 @@ public class Menu
 
         Scanner user = new Scanner(System.in);
         System.out.println("Please type in the number below that correlates with the chart above to get more information:");
-        int foodAnswer = user.nextInt();
+        String foodAnswer = user.nextLine();
+        String foodString;
+        switch (foodAnswer) {
+            case 1:
+                foodString = "2mato";
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            default: foodString = "Invalid input";
+                break;
+        }
+                System.out.println(foodString);
 
     //switch statement here that gives certain information on places based on the number the user inputs
     }
