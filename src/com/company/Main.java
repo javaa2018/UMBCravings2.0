00b3f799adaa293d7a048de7d9986eb48ade2dec
teamET147 <43768088+teamET147@users.noreply.github.com>
2018-11-29 13:30:09 -0500

    printing the directions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,9 +13,14 @@ public class Main
     public static void main(String[] args)
     {
 
-
-        System.out.println("Please answer the following questions yes or no:");
+// below is printing the directions along with a brief description of the program itself
+        System.out.println("Welcome to UMBCravings!");
+        System.out.println("This program will ask you several questions to help you decide where to eat during the current meal period.");
+        System.out.println("Please follow the directions below.");
+        System.out.println("Please carefully read the following questions, then answer either yes or no:");
         System.out.println("--------------------------");
+
+ //calling other classes and methods into main method in order to make the code easier to read
         glutenfree glutenfree= new glutenfree();
         glutenfree.gf(); //calling method
         Veggie veggie=new Veggie();
