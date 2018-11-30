40913b9c6a304b3f7b9417e031af47d55ee43f6d
Gabi <teamET147@gmail.com>
2018-11-29 23:01:05 -0500

    added

---
+++ src/com/company/Diet.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Diet {
+    //use setters and getters
+    //set the time and date
+    //dont' pull actual date
+    //have one default location for the constructor
+    //overload with all the different places
+    //look at birthday class for this in github!
+
+    //have location, zip..things to do with address and create a bunch of objects based on a certain class
+    //call the method through the loop
+    //problem a while loop
+    // employee.java file encapsulation
+}
