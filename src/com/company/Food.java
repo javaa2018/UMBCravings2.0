cc2a69a3fc3584c638401a50311134f29af2a8eb
Gabi <teamET147@gmail.com>
2018-12-04 14:25:29 -0500

    changed name from diet class to food class

---
+++ src/com/company/Food.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class Food
+{
+    /*
+     *vegetarian, glutenfree call in here, call the diet class into the main class
+     * something.glutenFree
+     *
+     *from employee class has encapsulation
+     *data atribute of employees
+     *
+     * have public abstract class estName{  put in separate class!! will have some of this information
+     * method overloading in here for food place names
+     *
+
+
+    private String phoneNumber; //put default phone number, why is it in grey is it becuase of phone number?
+    private Address address; //have default location, something is wrong with address
+    private String restaurantName; // restuarant name
+
+        class Diet(String phoneNumber, Address address, restaurantName)
+    {  //constructor w/ no return type same name as class, want to call in MAIN somehow!!
+            phoneNumber = phone; //figure out why phone numberi s red??
+            //calls program in another file
+            this.address = address; //not sure if we are going to put actual address or where it is in commons? Will fill out later
+            this.phoneNumber = phoneNumber;
+            this.restaurantName = restaurantName;
+        }
+
+        public Address getAddress() {
+            return phoneNumber;
+        }
+
+        public void setAddress(Address address) { //do i have to set address for all of them becuase they are different mightjjust do method overloading
+            this.address = address;
+        }
+
+        public String getPhone() { //name different?
+            return phoneNumber;
+        }
+
+        public void setPhone(String value) {
+            phoneNumber = value;
+        }
+
+
+        public String getPlaceName() {
+            return restaurantName; //why is the value wrong
+        }
+
+        public void setPlaceName(String value) {
+            phoneNumber = value;
+        }
+    }
+
+    public void printFood()  //data encapsulation
+    //will print the default place to eat for the diet class or cosntructor overload
+    {
+        System.out.println(address+restaurantName+phoneNumber); //will print out the address restuarnat name and phone number
+        System.out.println();
+    }
+*/
+
+}
+    //have method overloading down here
+    //set the time and date
+    //don't pull actual date
+    //have one default location for the constructor
+    //overload with all the different places
+    //look at birthday class for this in github!
+
+    //have location, zip..things to do with address and create a bunch of objects based on a certain class
+    //call the method through the loop
+    //maybe have a while loop
+    // employee.java file encapsulation
+
+
+
+
